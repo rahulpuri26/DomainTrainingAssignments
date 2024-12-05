21e2fda4c4febf7671ef1aef4ea0224c3e9447f8
Rahul Puri <83388231+rahulpuri26@users.noreply.github.com>
2024-12-05 10:57:08 +0530

    Add files via upload

---
+++ CustomMiddleware/CustomMiddleware/Middleware/CustomMiddleware.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class CustomMiddleware1
+{
+    private readonly RequestDelegate _next;
+
+    public CustomMiddleware1(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Custom logic before calling the next middleware
+        Console.WriteLine("Custom Middleware: Before Request");
+
+        // Call the next middleware in the pipeline
+        await _next(context);
+
+        // Custom logic after calling the next middleware
+        Console.WriteLine("Custom Middleware: After Request");
+    }
+}
---
+++ CustomMiddleware/CustomMiddleware/Middleware/CustomMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CustomMiddleware.MiddleWare
+{
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware1>();
+        }
+    }
+
+}
